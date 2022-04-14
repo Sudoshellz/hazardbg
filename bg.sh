e01675138b5e943cda97c76a887fedb05090c5e8
Sudoshellz <100945110+Sudoshellz@users.noreply.github.com>
2022-04-14 11:19:27 -0400

    Update bg.sh

--- bg.sh
+++ bg.sh
@@ -3,4 +3,4 @@ read -p "State: " State
 read -p "BirthYear: " Year
 
 curl --request GET \
-     --url 'https://api.peopledatalabs.com/v5/person/enrich?first_name='$Name'%20name&region='$State'&birth_date='$Year'&min_likelihood=0&include_if_matched=false&titlecase=true&pretty=true&api_key=97ed82f92e122b736b00039f40323b7d5fb8e30b5fcaa6629db0788544026c97'
+     --url 'https://api.peopledatalabs.com/v5/person/enrich?first_name=$Name%20name&region=$State&birth_date=$Year&min_likelihood=0&include_if_matched=false&titlecase=true&pretty=true&api_key=97ed82f92e122b736b00039f40323b7d5fb8e30b5fcaa6629db0788544026c97'
